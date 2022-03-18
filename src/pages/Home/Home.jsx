03f088d2a93729715f5a153c32bad80d5a02cd0b
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-03-18 22:59:38 +0530

    refactor: add assets to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,60 +1,68 @@
+import {
+  dessertImg,
+  healthyImg,
+  meatSkewerImg,
+  pastaImg,
+} from "../../assets/images";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <main class="home-wrapper">
-      <section class="hero overlay">
-        <div class="page-intro">
+    <main className="home-wrapper">
+      <section className="hero overlay">
+        <div className="page-intro">
           <article>
-            <h3 class="intro-heading">Good Food, Good Mood</h3>
-            <p class="intro-sub-heading">Save 60% on your first order</p>
-            <button class="button btn-outlined intro-btn">Order Now</button>
+            <h3 className="intro-heading">Good Food, Good Mood</h3>
+            <p className="intro-sub-heading">Save 60% on your first order</p>
+            <button className="button btn-outlined intro-btn">Order Now</button>
           </article>
         </div>
       </section>
 
-      <section class="intro-blocks">
-        <ul class="blocks">
-          <li class="block">
+      <section className="intro-blocks">
+        <ul className="blocks">
+          <li className="block">
             <article>
               <div>
-                <i class="fas fa-money-bill-alt"></i>
+                <i className="fas fa-money-bill-alt"></i>
               </div>
 
-              <h6 class="block-heading">No Minimum Order</h6>
-              <p class="block-para">
+              <h6 className="block-heading">No Minimum Order</h6>
+              <p className="block-para">
                 Order for yourself or for your group with no restrictions on
                 order value.
               </p>
             </article>
           </li>
-          <li class="block">
+          <li className="block">
             <article>
               <div>
-                <i class="fas fa-truck-loading"></i>
+                <i className="fas fa-truck-loading"></i>
               </div>
-              <h6 class="block-heading">Express Delivery</h6>
-              <p class="block-para">
+              <h6 className="block-heading">Express Delivery</h6>
+              <p className="block-para">
                 Get your food delivered fresh and on time.
               </p>
             </article>
           </li>
-          <li class="block">
+          <li className="block">
             <article>
               <div>
-                <i class="fas fa-map-marked"></i>
+                <i className="fas fa-map-marked"></i>
               </div>
-              <h6 class="block-heading">Track Order</h6>
-              <p class="block-para">Know where your order is at all times!</p>
+              <h6 className="block-heading">Track Order</h6>
+              <p className="block-para">
+                Know where your order is at all times!
+              </p>
             </article>
           </li>
-          <li class="block">
+          <li className="block">
             <article>
               <div>
-                <i class="fas fa-cash-register"></i>
+                <i className="fas fa-cash-register"></i>
               </div>
-              <h6 class="block-heading">Best Deals</h6>
-              <p class="block-para">
+              <h6 className="block-heading">Best Deals</h6>
+              <p className="block-para">
                 Get best deals and offers on your favourite food items.
               </p>
             </article>
@@ -62,19 +70,19 @@ const Home = () => {
         </ul>
       </section>
 
-      <h1 class="categories-heading">Featured Categories</h1>
-      <section class="feat-categories">
-        <div class="category">
-          <div class="category-image">
-            <img src="/assets/images/meat-skewer.jpg" alt="category" />
+      <h1 className="categories-heading">Featured Categories</h1>
+      <section className="feat-categories">
+        <div className="category">
+          <div className="category-image">
+            <img src={meatSkewerImg} alt="category" />
           </div>
-          <div class="category-content">
-            <div class="content-shown">
+          <div className="category-content">
+            <div className="content-shown">
               <h4>Indian Food</h4>
               <p>Delicacies of India</p>
             </div>
 
-            <div class="content-hidden">
+            <div className="content-hidden">
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Incidunt voluptate pariatur id adipisci quidem nemo, quia dolore
@@ -83,17 +91,17 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div class="category">
-          <div class="category-image">
-            <img src="/assets/images/healthy.jpg" alt="category" />
+        <div className="category">
+          <div className="category-image">
+            <img src={healthyImg} alt="category" />
           </div>
-          <div class="category-content">
-            <div class="content-shown">
+          <div className="category-content">
+            <div className="content-shown">
               <h4>Healthy</h4>
               <p>For the fitness lovers</p>
             </div>
 
-            <div class="content-hidden">
+            <div className="content-hidden">
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Incidunt voluptate pariatur id adipisci quidem nemo, quia dolore
@@ -102,17 +110,17 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div class="category">
-          <div class="category-image">
-            <img src="/assets/images/desserts.jpg" alt="category" />
+        <div className="category">
+          <div className="category-image">
+            <img src={dessertImg} alt="category" />
           </div>
-          <div class="category-content">
-            <div class="content-shown">
+          <div className="category-content">
+            <div className="content-shown">
               <h4>Desserts</h4>
               <p>For the ones with a sweet tooth</p>
             </div>
 
-            <div class="content-hidden">
+            <div className="content-hidden">
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Incidunt voluptate pariatur id adipisci quidem nemo, quia dolore
@@ -121,17 +129,17 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div class="category">
-          <div class="category-image">
-            <img src="/assets/images/pizza.jpg" alt="category" />
+        <div className="category">
+          <div className="category-image">
+            <img src={pastaImg} alt="category" />
           </div>
-          <div class="category-content">
-            <div class="content-shown">
+          <div className="category-content">
+            <div className="content-shown">
               <h4>Fast Food</h4>
               <p>For the cheat days</p>
             </div>
 
-            <div class="content-hidden">
+            <div className="content-hidden">
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Incidunt voluptate pariatur id adipisci quidem nemo, quia dolore
