391f6561e843f917c84b1b23c49923566cf7484c
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-03-20 13:25:44 +0530

    feat: export images from a single file

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,64 @@
+// Banner images
 import pizzaImg from "./pizza.jpg";
 import meatSkewerImg from "./meat-skewer.jpg";
 import healthyImg from "./healthy.jpg";
 import dessertImg from "./desserts.jpg";
 import indianFoodImg from "./indian-food.jpg";
 import pastaImg from "./pasta.jpg";
+
+// Products images
+
+// Bakery Products
+import chocolateCake from "./bakery/chocolate-cake-4.jpg";
+import cinnamonRoll from "./bakery/cinnamon-roll-2.jpg";
+import cookies from "./bakery/cookies-3.jpg";
+import croissantImg from "./bakery/croissants-1.jpg";
+import muffinImg from "./bakery/muffins-5.jpg";
+
+// Desserts images
+import chocoSundae from "./desserts/choco-sundae-4.jpg";
+import donutImg from "./desserts/donuts-3.jpg";
+import iceCream from "./desserts/ice-cream-5.jpg";
+import vanillaSundae from "./desserts/vanilla-sundae-2.jpg";
+import wafflesImg from "./desserts/waffles-1.jpg";
+
+// Fast food images
+import chickenBurger from "./fast-food/chicken-burger-1.jpg";
+import chickenNuggets from "./fast-food/chicken-nuggets-2.jpg";
+import chickenPizza from "./fast-food/chicken-pizza-9.jpg";
+import chickenWings from "./fast-food/chicken-wings-6.jpg";
+import frenchFries from "./fast-food/french-fries-4.jpg";
+import hotDog from "./fast-food/hot-dog-7.jpg";
+import margheritaPizza from "./fast-food/margherita-pizza-8.jpg";
+import onionRings from "./fast-food/onion-rings-5.jpg";
+import vegBurger from "./fast-food/veg-burger-3.jpg";
+
+// Healthy food images
+import avocadoToast from "./healthy/avocado-toast-healthy-6.jpg";
+import chickenVegSalad from "./healthy/chicken-veggie-salad-3.jpg";
+import clubHouseSandwich from "./healthy/clubhouse-sandwich-healthy-7.jpg";
+import fruitNutSalad from "./healthy/fruit-nut-salad-2.jpg";
+import fruitSalad from "./healthy/fruit-salad-1.jpg";
+import grilledChicken from "./healthy/grilled-chicken-healthy-4.jpg";
+import pastaSalad from "./healthy/pesto-pasta-salad-5.jpg";
+
+// Juice images
+import kiwiJuice from "./juices/kiwi-juice-3.jpg";
+import orangeJuice from "./juices/orange-juice-1.jpg";
+import watermelonJuice from "./juices/watermelon-juice-2.jpg";
+
+// Indian food images
+import butterChicken from "./indian/butter-chicken-8.jpg";
+import butterNaan from "./indian/butter-naan-10.jpg";
+import chickenBiryani from "./indian/chicken-biryani-6.jpg";
+import gajarHalwa from "./indian/gajar-halwa-5.jpg";
+import idliChutney from "./indian/idli-chutney-4.jpg";
+import masalaDosa from "./indian/masala-dosa-7.jpg";
+import paneerTikkaMasala from "./indian/paneer-tikka-masala-11.jpg";
+import pavBhaji from "./indian/pav-bhaji-2.jpg";
+import sabudanaKhichdi from "./indian/sabudana-khichdi-9.jpg";
+import samosaImg from "./indian/samosa-3.jpg";
+import vegThali from "./indian/veg-thali-1.jpg";
 
 export {
   pizzaImg,
@@ -12,4 +67,44 @@ export {
   dessertImg,
   indianFoodImg,
   pastaImg,
+  chocolateCake,
+  cinnamonRoll,
+  cookies,
+  croissantImg,
+  muffinImg,
+  chocoSundae,
+  donutImg,
+  iceCream,
+  vanillaSundae,
+  wafflesImg,
+  chickenBurger,
+  chickenNuggets,
+  chickenPizza,
+  chickenWings,
+  frenchFries,
+  hotDog,
+  margheritaPizza,
+  onionRings,
+  vegBurger,
+  avocadoToast,
+  chickenVegSalad,
+  clubHouseSandwich,
+  fruitNutSalad,
+  fruitSalad,
+  grilledChicken,
+  pastaSalad,
+  kiwiJuice,
+  orangeJuice,
+  watermelonJuice,
+  butterChicken,
+  butterNaan,
+  chickenBiryani,
+  gajarHalwa,
+  idliChutney,
+  masalaDosa,
+  paneerTikkaMasala,
+  pavBhaji,
+  sabudanaKhichdi,
+  samosaImg,
+  vegThali,
 };
