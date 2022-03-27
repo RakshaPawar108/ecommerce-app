8eac79c1c8e536b40ee5da7ef69d63354852628a
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-03-27 11:54:03 +0530

    feat: add auth context

---
+++ src/context/auth-context.jsx
@@ -0,0 +1,11 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export {AuthProvider, useAuth}
