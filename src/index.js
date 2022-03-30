89377c4eaa7edb3a0846881ae013ff1ee04b3abb
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-03-30 19:55:38 +0530

    feat: add cart context

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProvider, FilterProvider } from "./context";
+import { AuthProvider, CartProvider, FilterProvider } from "./context";
 
 // Call make Server
 makeServer();
@@ -12,11 +12,13 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <AuthProvider>
-        <FilterProvider>
-          <App />
-        </FilterProvider>
-      </AuthProvider>
+      <CartProvider>
+        <AuthProvider>
+          <FilterProvider>
+            <App />
+          </FilterProvider>
+        </AuthProvider>
+      </CartProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
