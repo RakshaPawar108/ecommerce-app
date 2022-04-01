72957cb0d541925e5fb1b9b540b9d62922f6bd10
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-04-01 13:13:58 +0530

    feat: add wishlist context and wishlist reducer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProvider, CartProvider, FilterProvider } from "./context";
+import {
+  AuthProvider,
+  CartProvider,
+  FilterProvider,
+  WishlistProvider,
+} from "./context";
 
 // Call make Server
 makeServer();
@@ -12,13 +17,15 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <CartProvider>
-        <AuthProvider>
-          <FilterProvider>
-            <App />
-          </FilterProvider>
-        </AuthProvider>
-      </CartProvider>
+      <WishlistProvider>
+        <CartProvider>
+          <AuthProvider>
+            <FilterProvider>
+              <App />
+            </FilterProvider>
+          </AuthProvider>
+        </CartProvider>
+      </WishlistProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/reducers/wishlist-reducer.jsx
@@ -0,0 +1,17 @@
+const wishlistReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_WISHLIST":
+      return { ...state, wishlist: action.payload };
+
+    case "REMOVE_FROM_WISHLIST":
+      return { ...state, wishlist: action.payload };
+
+    case "GET_INITIAL_WISHLIST":
+      return { ...state, wishlist: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export { wishlistReducer };
