d7dd4595e105d8ccbe42cc3933ac889a031e4ceb
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-04-09 11:14:18 +0530

    feat: add loader to wishlist page and set footer to bottom

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { WishlistItems } from "../../components";
+import { useEffect, useState } from "react";
+import { Loader, WishlistItems } from "../../components";
 import { useWishlist, useAuth } from "../../context";
 import { fetchInitialWishlistService } from "../../services";
 import "./Wishlist.css";
@@ -9,11 +9,13 @@ export const Wishlist = () => {
   const {
     authState: { token },
   } = useAuth();
+  const [loading, setLoading] = useState(true);
 
   const getInitialWishlist = async () => {
     try {
       const response = await fetchInitialWishlistService(token);
       if (response.status === 200) {
+        setLoading(false);
         wishlistDispatch({
           type: "GET_INITIAL_WISHLIST",
           payload: response.data.wishlist,
@@ -32,15 +34,18 @@ export const Wishlist = () => {
 
   return (
     <main className="wishlist-wrapper">
+      {loading && <Loader />}
       <h3 className="wishlist-heading">My Wishlist</h3>
       <small className="num-items">{wishlistState.wishlist.length} Items</small>
-      <>
-        {wishlistState.wishlist.length > 0 ? (
-          <WishlistItems />
-        ) : (
-          <h1>Your Wishlist is Empty</h1>
-        )}
-      </>
+      {!loading ? (
+        <>
+          {wishlistState.wishlist.length > 0 ? (
+            <WishlistItems />
+          ) : (
+            <h1>Your Wishlist is Empty</h1>
+          )}
+        </>
+      ) : null}
     </main>
   );
 };
